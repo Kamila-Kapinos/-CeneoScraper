9902ba83232ca676a5032bbe81c7fa5d63e2e326
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2022-04-08 15:18:40 +0200

    2022-04-08 KK edit2

--- scraper.py
+++ scraper.py
@@ -16,12 +16,11 @@ author = opinion.select_one("span.user-post__author-name").get_text().strip()
 recommendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
 stars = opinion.select_one("span.user-post__score-count").get_text().strip()
 content = opinion.select_one("div.user-post__text").get_text().strip()
+
 useful = opinion.select_one('span[id^="votes-yes"]').get_text().strip()
 useless = opinion.select_one('span[id^="votes-no"]').get_text().strip()
-
 date_time = opinion.select_one('span.user-post__published > time:nth-child(1)')["datetime"]
 purchase_date = opinion.select_one('span.user-post__published > time:nth-child(2)')["datetime"]
-
 
 
 print(recommendation, stars, content, type(useless), type(useful), type(date_time), type(purchase_date))
